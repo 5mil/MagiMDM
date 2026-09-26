2085e381852874f154daf007771502e493e7a99d
5mil <5mil@users.noreply.github.com>
2026-09-26 12:27:59 -0400

    Copy UI into src/ so @embedFile is inside the package

--- src/pages.zig
+++ src/pages.zig
@@ -1,6 +1,6 @@
-pub const home = @embedFile("../web/home.html");
-pub const lms = @embedFile("../web/lms.html");
-pub const school = @embedFile("../web/school.html");
-pub const comms = @embedFile("../web/comms.html");
-pub const enroll_pc = @embedFile("../web/enroll_pc.html");
-pub const algebra_war = @embedFile("../web/algebra_war.html");
+pub const home = @embedFile("home.html");
+pub const lms = @embedFile("lms.html");
+pub const school = @embedFile("school.html");
+pub const comms = @embedFile("comms.html");
+pub const enroll_pc = @embedFile("enroll_pc.html");
+pub const algebra_war = @embedFile("algebra_war.html");
