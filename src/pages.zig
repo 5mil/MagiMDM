7c53be85ee80edc03edb0020091938cfda4824bc
5mil <5mil@users.noreply.github.com>
2026-09-26 12:24:16 -0400

    Embed web UI so / is not a one-line fallback

---
+++ src/pages.zig
@@ -0,0 +1,6 @@
+pub const home = @embedFile("../web/home.html");
+pub const lms = @embedFile("../web/lms.html");
+pub const school = @embedFile("../web/school.html");
+pub const comms = @embedFile("../web/comms.html");
+pub const enroll_pc = @embedFile("../web/enroll_pc.html");
+pub const algebra_war = @embedFile("../web/algebra_war.html");
